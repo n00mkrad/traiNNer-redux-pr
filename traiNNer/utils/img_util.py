8384cba901d12245e31460077477a221c270b99f
n00mkrad <61149547+n00mkrad@users.noreply.github.com>
2026-03-21 21:48:03 +0100

    Fix srgb vipsimfrompath bit depth

--- traiNNer/utils/img_util.py
+++ traiNNer/utils/img_util.py
@@ -328,9 +328,9 @@ def imfrombytes(content: bytes, flag: str = "color", float32: bool = False) -> M
 
 
 def vipsimfrompath(path: str) -> pyvips.Image:
-    img = pyvips.Image.new_from_file(
-        path, access="sequential", fail=True
-    ).icc_transform("srgb")  # pyright: ignore[reportAttributeAccessIssue,reportOptionalMemberAccess]
+    img = pyvips.Image.new_from_file(path, access="sequential", fail=True)
+    depth = 16 if img.format in ("ushort", "short") else 8
+    img = img.icc_transform("srgb", depth=depth)  # pyright: ignore[reportAttributeAccessIssue,reportOptionalMemberAccess]
     assert isinstance(img, pyvips.Image)
     return img
 
